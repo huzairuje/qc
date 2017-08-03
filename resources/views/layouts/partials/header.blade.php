d1282c5559745c555bd5bf88750ac091e7b77c9e
ujehuzair <metalh8@gmail.com>
2017-08-03 16:52:49 +0700

    add favicon(title) and delete right sidebar

--- resources/views/layouts/partials/header.blade.php
+++ resources/views/layouts/partials/header.blade.php
@@ -22,7 +22,6 @@
                     <!-- Call Search -->
                    
                     <!-- #END# Tasks -->
-                    <li class="pull-right"><a href="javascript:void(0);" class="js-right-sidebar" data-close="true"><i class="material-icons">more_vert</i></a></li>
                 </ul>
             </div>
         </div>
