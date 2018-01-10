9cf9340a314ac6a320e5155fb836487e9457b91a
huzairuje <metalh8@gmail.com>
2018-01-10 14:33:48 +0700

    fix calon input has_wakil v1

--- resources/views/layouts/data_master/calon/create.blade.php
+++ resources/views/layouts/data_master/calon/create.blade.php
@@ -29,89 +29,9 @@
 
                                 <div class="form-group">
                                     <div class="row">
-                                        <div class="col-md-4">
-                                            <div class="form-line">
-                                                {!! Form::label('tipe', 'Tipe:') !!}
-                                                <select id="tipe" name="tipe" class="form-control show-tick">
-                                                    <option value="0">Partai</option>
-                                                    <option value="1">Angota</option>
-                                                </select>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-
-                                <div class="form-group">
-                                    <div class="row">
-                                        <div class="col-md-4">
-                                            <div class="form-line">
-                                                {!! Form::select('partai_id', $partai,null, ['class' => 'form-control','id' => 'partai_id','placeholder' => 'Pilih Partai']) !!}
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-
-                                <div class="form-group nama-form-container">
-                                    <div class="row">
-                                        <div class="col-md-4">
-                                            <div class="form-line">
-                                                {!! Form::label('nama', 'Nama:') !!}
-                                                {{ Form::text('nama',null, ['class' => 'form-control','placeholder' => 'Nama']) }}
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-
-                                <div class="form-group nama-form-container">
-                                    <div class="row">
-                                        <div class="col-md-4">
-                                            <div class="form-line">
-                                                {!! Form::label('nomor', 'Nomor Urut:') !!}
-                                                {{ Form::text('nomor',null, ['class' => 'form-control','placeholder' => 'Nomor Urut']) }}
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-
-                                <div class="form-group">
-                                    <div class="row">
-                                        <div class="col-md-4">
-                                            <div class="form-line">
-                                                {!! Form::label('alamat', 'Alamat:') !!}
-                                                {{ Form::text('alamat',null, ['class' => 'form-control','placeholder' => 'Alamat Lengkap']) }}
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-
-                                <div class="form-group">
-                                    <div class="row">
-                                        <div class="col-md-4">
-                                            <div class="form-line">
-                                                {!! Form::label('no_telpon', 'Nomor Telepon:') !!}
-                                                {{ Form::number('no_telpon',null, ['class' => 'form-control','placeholder' => 'Nomor Telepon']) }}
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-
-                                <div class="form-group">
-                                    <div class="row">
-                                        <div class="col-md-4">
-                                            <div class="form-line">
-                                                {!! Form::label('email', 'Email :') !!}
-                                                {{ Form::email('email',null, ['class' => 'form-control','placeholder' => 'example@example.com']) }}
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-
-                                <div class="form-group">
-                                    <div class="row">
-                                        <div class="col-md-4">
+                                        <div class="col-md-8">
                                             <div class="form-line">
                                                 {!! Form::label('event', 'Pilih Event:') !!}
-                                                <br />
                                                 <select id="event_id" name="event" class="forrm-control show-tick">
                                                     <option value="">Pilih Event</option>
                                                     @foreach($listEvent as $event)
@@ -125,7 +45,7 @@
 
                                 <div class="form-group">
                                     <div class="row">
-                                        <div class="col-md-4">
+                                        <div class="col-md-8">
                                             <div class="form-line">
                                                 {!! Form::label('dapil_id', 'Pilih Dapil:') !!}
                                                 {{ Form::select('dapil_id', $dapil, ['class' => 'form-control','id' => 'dapil','placeholder' => 'Pilih Dapil']) }}
@@ -136,7 +56,114 @@
 
                                 <div class="form-group">
                                     <div class="row">
-                                        <div class="col-md-4">
+                                        <div class="col-md-8">
+                                            <div class="form-line">
+                                                {!! Form::label('tipe', 'Tipe:') !!}
+                                                <select id="tipe" name="tipe" class="form-control show-tick">
+                                                    <option value="0">Partai</option>
+                                                    <option value="1">Anggota</option>
+                                                </select>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div class="form-group">
+                                    <div class="row">
+                                        <div class="col-md-8">
+                                            <div class="form-line">
+                                                {!! Form::label('partai_id', 'Pilih Partai:') !!}
+                                                {!! Form::select('partai_id', $partai,null, ['class' => 'form-control','id' => 'partai_id','placeholder' => 'Pilih Partai']) !!}
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div class="form-group nama-form-container">
+                                    <div class="row">
+                                        <div class="col-md-10">
+                                            <div class="form-line">
+                                                {!! Form::label('has_wakil', 'FORM CALON') !!}
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div class="form-group nama-form-container">
+                                    <div class="row">
+                                        <div class="col-md-8">
+                                            <div class="form-line">
+                                                {!! Form::label('has_wakil', 'Mempunyai Wakil:') !!}
+                                                <select id="has_wakil" name="has_wakil" class="form-control show-tick">
+                                                    <option value="0">Tidak</option>
+                                                    <option value="1">Ya</option>
+                                                </select>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div class="form-group nama-form-container">
+                                    <div class="row">
+                                        <div class="col-md-8">
+                                            <div class="form-line">
+                                                {!! Form::label('nama', 'Nama:') !!}
+                                                {{ Form::text('nama',null, ['class' => 'form-control','placeholder' => 'Nama']) }}
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+
+
+
+                                <div class="form-group nama-form-container">
+                                    <div class="row">
+                                        <div class="col-md-8">
+                                            <div class="form-line">
+                                                {!! Form::label('nomor', 'Nomor Urut:') !!}
+                                                {{ Form::text('nomor',null, ['class' => 'form-control','placeholder' => 'Nomor Urut']) }}
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div class="form-group">
+                                    <div class="row">
+                                        <div class="col-md-8">
+                                            <div class="form-line">
+                                                {!! Form::label('alamat', 'Alamat:') !!}
+                                                {{ Form::text('alamat',null, ['class' => 'form-control','placeholder' => 'Alamat Lengkap']) }}
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div class="form-group">
+                                    <div class="row">
+                                        <div class="col-md-8">
+                                            <div class="form-line">
+                                                {!! Form::label('no_telpon', 'Nomor Telepon:') !!}
+                                                {{ Form::number('no_telpon',null, ['class' => 'form-control','placeholder' => 'Nomor Telepon']) }}
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div class="form-group">
+                                    <div class="row">
+                                        <div class="col-md-8">
+                                            <div class="form-line">
+                                                {!! Form::label('email', 'Email :') !!}
+                                                {{ Form::email('email',null, ['class' => 'form-control','placeholder' => 'example@example.com']) }}
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+
+
+                                <div class="form-group">
+                                    <div class="row">
+                                        <div class="col-md-8">
                                             <div class="form-line">
                                                 {!! Form::label('foto', 'Foto :') !!}
                                                 <div id="wrapper">
@@ -150,6 +177,95 @@
                                         </div>
                                     </div>
                                 </div>
+
+                                <div class="form-wakil" style="display:none">
+                                <div class="form-group wakil-form-container">
+                                    <div class="row">
+                                        <div class="col-md-10">
+                                            <div class="form-line">
+                                                {!! Form::label('has_wakil', 'FORM CALON WAKIL') !!}
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div class="form-group wakil-form-container">
+                                    <div class="row">
+                                        <div class="col-md-8">
+                                            <div class="form-line">
+                                                {!! Form::label('nama', 'Nama:') !!}
+                                                {{ Form::text('nama',null, ['class' => 'form-control','placeholder' => 'Nama']) }}
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+
+
+
+                                <div class="form-group wakil-form-container">
+                                    <div class="row">
+                                        <div class="col-md-8">
+                                            <div class="form-line">
+                                                {!! Form::label('nomor', 'Nomor Urut:') !!}
+                                                {{ Form::text('nomor',null, ['class' => 'form-control','placeholder' => 'Nomor Urut']) }}
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div class="form-group wakil-form-container">
+                                    <div class="row">
+                                        <div class="col-md-8">
+                                            <div class="form-line">
+                                                {!! Form::label('alamat', 'Alamat:') !!}
+                                                {{ Form::text('alamat',null, ['class' => 'form-control','placeholder' => 'Alamat Lengkap']) }}
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div class="form-group wakil-form-container">
+                                    <div class="row">
+                                        <div class="col-md-8">
+                                            <div class="form-line">
+                                                {!! Form::label('no_telpon', 'Nomor Telepon:') !!}
+                                                {{ Form::number('no_telpon',null, ['class' => 'form-control','placeholder' => 'Nomor Telepon']) }}
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div class="form-group wakil-form-container">
+                                    <div class="row">
+                                        <div class="col-md-8">
+                                            <div class="form-line">
+                                                {!! Form::label('email', 'Email :') !!}
+                                                {{ Form::email('email',null, ['class' => 'form-control','placeholder' => 'example@example.com']) }}
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+
+
+                                <div class="form-group wakil-form-container">
+                                    <div class="row">
+                                        <div class="col-md-8">
+                                            <div class="form-line">
+                                                {!! Form::label('foto', 'Foto :') !!}
+                                                <div id="wrapper">
+                                                    <style type="text/css">.thumb-image{float:left;width:200px;position:relative;padding:5px;}</style>
+                                                    <div id="image-holder">
+                                                    </div>
+                                                    <br>
+                                                    {!! Form::file('foto', null, ['class' => 'form-control']) !!}
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                              </div>
+
+
 
                                 <div class="form-group">
                                     <div class="form-line">
@@ -176,9 +292,26 @@
         var _val = $(this).val();
         if(_val == 0){
             $('.nama-form-container').hide();
+
+            $('.form-wakil').hide();
         }
         else {
             $('.nama-form-container').show();
+
+            if ($('#has_wakil').val() == 1) {
+              $('.form-wakil').show();
+            }
+        }
+    });
+
+    $(document).on('change','#has_wakil',function(){
+        var _val = $(this).val();
+        if(_val == 0){
+            $('.form-wakil').hide();
+        }
+        else {
+            $('.form-wakil').show();
+
         }
     });
 
@@ -199,4 +332,5 @@
     });
 });
 </script>
+
 @endsection
